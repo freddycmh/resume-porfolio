13eeda448b4fe7f9b9fd93bd75c0279a6d39fc40
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-06-17 13:17:04 -0400

    fixed logo and title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -41,10 +42,10 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={Logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Adrian &nbsp;
-            <span className="sm:block hidden"> | JavaScript Mastery</span>
+            Freddy &nbsp;
+            <span className="sm:block hidden"> | Mok Ho</span>
           </p>
         </Link>
 
